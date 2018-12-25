2b27d39839ac143bcfe71766a20fff35b8918b75
zxshiwo <2235110071@qq.com>
2018-12-25 09:29:26 +0800

    shanchu

--- datasapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class User(models.Model):
-    id= models.AutoField(primary_key=True)
-    name= models.CharField(max_length=100)
-    passwd = models.CharField(max_length=60)

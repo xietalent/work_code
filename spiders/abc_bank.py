a48b081dcc70139c860d193862159c284064df56
zxshiwo <2235110071@qq.com>
2019-02-13 18:09:52 +0800

    '13/2'

--- spiders/abc_bank.py
+++ spiders/abc_bank.py
@@ -1,4 +1,5 @@
 
+from threading import Thread
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -16,7 +17,7 @@ import time
 import pytesseract
 import pytesseract.pytesseract
 import requests
-
+import pymysql
 
 class abc_bank(object):
     def __init__(self,timeout=None,service_args=[]):
@@ -103,7 +104,6 @@ class abc_bank(object):
                     print("卡类型为:{}".format(card_type))
                     print("卡号为:{}".format(card_nums))
                     print("卡积分数为:{}".format(card_score))
-
             except:
                 pass
 
@@ -166,9 +166,100 @@ class abc_bank(object):
         except:
             pass
 
+    #获取借记卡明细
+    def detail(self):
+        sleep(0.1)
+        self.browser.find_element_by_xpath(".//ul[@class='tabs-head']/li[1]/a").click()
+        sleep(1.5)
+        self.browser.find_element_by_class_name("minxi").click()
+        sleep(2)
+        self.browser.find_element_by_id("startDate").click()
+        sleep(0.2)
+        for nums in range(11):
+            self.browser.find_element_by_class_name("pika-prev").click()
+            sleep(0.1)
+        sleep(0.5)
+
+        self.browser.find_element_by_xpath(".//div[@class='pika-lendar']/table[@class='pika-table']/tbody/tr[2]/td[1]/button").click()
+        sleep(0.5)
+        self.browser.find_element_by_id("btn_query").click()
+        sleep(3)
+        # nums = 1
+        def all_info(nums):
+            print("获取了第{}页信息".format(nums))
+            info_html = self.browser.page_source
+            info_tree = etree.HTML(info_html)
+
+            divs = info_tree.xpath(".//tbody[@id='AccountTradeDetailTable']")
+            info_items = []
+            for j in range(1,15):
+                try:
+                    for div in divs:
+                        info_item= {}
+                        trans_date = div.xpath("./tr[{}]/td[1]/div[1]/text()".format(j))[0].strip()
+                        trans_time =div.xpath("./tr[{}]/td[1]/div[2]/text()".format(j))[0].strip()
+                        #交易金额
+                        trans_amount = div.xpath("./tr[{}]/td[2]/text()".format(j))[0].strip()
+                        #本次余额
+                        balance= div.xpath("./tr[{}]/td[3]/text()".format(j))[0].strip()
+                        # 对方信息
+                        # other_side = div.xpath("./tr[{}]/td[4]/div[1]/text()".format(j))[0].strip()
+                        other_side = div.xpath("./tr[{}]/td[4]/div[2]/text()".format(j))[0].strip()
+                        #交易类型
+                        trans_type = div.xpath("./tr[{}]/td[5]/text()".format(j))[0].strip()
+                        #交易渠道
+                        trans_channel = div.xpath("./tr[{}]/td[6]/text()".format(j))[0].strip()
+                        #交易摘要
+                        trans_summary = div.xpath("./tr[{}]/td[7]/text()".format(j))[0].strip()
+
+                        print("交易时间{}_{}".format(trans_date,trans_time))
+                        print("本次交易金额为:{}".format(trans_amount))
+                        print("本次交易后余额为:{}".format(balance))
+                        print("本次交易对方信息为:{}".format(other_side))
+                        # print("本次交易对方信息为:{}".format(other_side,other_side2))
+                        print("本次交易类型为:{}".format(trans_type))
+                        print("本次交易渠道为:{}".format(trans_channel))
+                        print("本次交易摘要为:{}".format(trans_summary))
+                        info_item = {
+                            "trans_date":trans_date,
+                            "trans_time":trans_time,
+                            "trans_amount":trans_amount,
+                            "balance":balance,
+                            "other_side":other_side,
+                            # "other_side2":other_side2,
+                            "trans_type":trans_type,
+                            "trans_channel":trans_channel,
+                            "trans_summary":trans_summary,
+                        }
+                        info_items.append(info_item)
+
+                        #数据库连接
+                        sql_conn = Mysql_input()
+                        th2 = Thread(target=sql_conn.set_data,args=(trans_date,trans_time,trans_amount,balance,other_side,trans_type,trans_channel,trans_summary,))
+                        th2.start()
+                except:
+                    pass
+                sleep(0.5)
+        nums = 1
+        res = all_info(nums)
+        while True:
+            try:
+                self.browser.find_element_by_id("nextPage").click()
+                sleep(1)
+                res = all_info(nums)
+                nums += 1
+                # sleep(1)
+            except:
+                print("已经获取一年内所有消费明细")
+                break
+                # pass
+
+    #获取存款信息
     def deposit_info(self):
+        #序号,存期,币种,金额,执行利率,开户日期,到期日,操作
 
         pass
+
 
     def user_info(self):
         username = "15071469916"
@@ -301,13 +392,69 @@ class abc_bank(object):
         tres4 = round(tres4,2)
         print("获取账户信息消耗时间:{}s".format(tres4))
 
+        #获取借记卡明细
+        self.detail()
+        t6 = time.time()
+        tres5 = t6 - t5
+        tres5 = round(tres5, 2)
+        print("查询借记卡明细耗时:{}s".format(tres5))
+
         #获取存款信息
         self.deposit_info()
-        t6 = time.time()
-        tres5 = t6-t5
-        tres5 = round(tres5,2)
-        print("查询存款信息耗时:{}s".format(tres5))
+        t7 = time.time()
+        tres6= t7-t6
+        tres6 = round(tres6,2)
 
+        print("登录请求耗时为:{}s".format(tres1))
+        print("获取信用卡信息耗时为:{}s".format(tres2))
+        print("获取信用卡账单耗时{}s".format(tres22))
+        print("获取卡片积分信息耗时:{}s".format(tres3))
+        print("获取账户信息消耗时间:{}s".format(tres4))
+        print("查询借记卡明细耗时:{}s".format(tres5))
+        print("查询存款信息耗时:{}s".format(tres6))
+
+
+
+class Mysql_input(object):
+    def __init__(self, host="47.97.217.36", user="root", password="root", database="user", port=3306, charset="utf8"):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.charset = charset
+
+    def connect(self):
+        self.conn = pymysql.connect(host=self.host, user=self.user, password=self.password, database=self.database,port=self.port, charset=self.charset)
+        self.cursor = self.conn.cursor()
+
+
+    def get_data(self):
+        self.connect()
+        sql_2 = "select * from abc_trans_recording order by id DESC limit 1;"
+        self.cursor.execute(sql_2)
+        res = self.cursor.fetchone()
+        return res
+
+    def set_data(self,trans_date,trans_time,trans_amount,balance,other_side,trans_type,trans_channel,trans_summary):
+        self.connect()
+        try:
+            # sql_1 = "INSERT INTO card_score VALUES(null,'{}','{}');".format(my_integral, bill)
+            sql_1 = "INSERT INTO abc_trans_recording VALUES(null,'{}','{}','{}','{}','{}','{}','{}','{}');".format(trans_date,trans_time,trans_amount,balance,other_side,trans_type,trans_channel,trans_summary)
+            self.cursor.execute(sql_1)
+            self.conn.commit()
+            print("添加成功")
+            res = self.cursor.fetchall()
+            if res != None:
+                self.close()
+                return res
+        except:
+            self.conn.rollback()
+
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
 
 if __name__ == '__main__':
     res = abc_bank()
